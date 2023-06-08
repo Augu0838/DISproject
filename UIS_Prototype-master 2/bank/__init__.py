3f6a38521fe271c4cab2d4c7ed95481b8b40eeb0
Augu0838 <54616820+Augu0838@users.noreply.github.com>
2023-06-08 09:53:20 +0200

    All kinds of stuff

--- UIS_Prototype-master 2/bank/__init__.py
+++ UIS_Prototype-master 2/bank/__init__.py
@@ -4,8 +4,8 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 #202212
-#from flask import session
-#from flask_session import Session
+# from flask import session
+# from flask_session import Session
 
 
 app = Flask(__name__)
@@ -20,26 +20,26 @@ conn = psycopg2.connect(db)
 bcrypt = Bcrypt(app)
 
 
-# login_manager = LoginManager(app)
-# login_manager.login_view = 'login'
-# login_manager.login_message_category = 'info'
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 #202212
 # Check Configuration section for more details
-#SESSION_TYPE = 'filesystem'
+SESSION_TYPE = 'filesystem'
 
 
 #202212
-# roles = ["ingen","employee","customer"]
+roles = ["ingen","employee","customer"]
 # print(roles)
-# mysession = {"state" : "initializing","role" : "Not assingned", "id": 0 ,"age" : 202212}
-# print(mysession)
+mysession = {"state" : "initializing","role" : "Not assingned", "id": 0 ,"age" : 202212}
+print(mysession)
 
-# from bank.Login.routes import Login
-# from bank.Customer.routes import Customer
-# from bank.Employee.routes import Employee
-# app.register_blueprint(Login)
-# app.register_blueprint(Customer)
-# app.register_blueprint(Employee)
+from bank.Login.routes import Login
+from bank.Customer.routes import Customer
+from bank.Employee.routes import Employee
+app.register_blueprint(Login)
+app.register_blueprint(Customer)
+app.register_blueprint(Employee)
 
 #from bank import routes
